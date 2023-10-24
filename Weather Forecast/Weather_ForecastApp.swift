32669be3bf02721b8c8a074aa1e66c061b39a2e7
AlexDevvx <116598908+AlexDevvx@users.noreply.github.com>
2023-10-24 09:34:24 -0400

    Initial Commit

---
+++ Weather Forecast/Weather_ForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_ForecastApp.swift
+//  Weather Forecast
+//
+//  Created by Consultant on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_ForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
